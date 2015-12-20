1625f17b686af9079c17cfd9849306105540c89f
logi85 <t.ribke@gmail.com>
2015-12-20 19:23:59 +0100

    windows support for setting login-cookie in chrome

--- resources/chrome_cookie.py
+++ resources/chrome_cookie.py
@@ -1,6 +1,14 @@
 import sqlite3
-from Crypto.Cipher import AES
-from Crypto.Protocol.KDF import PBKDF2
+import os
+import utility
+
+windows = os.name == 'nt'
+
+if windows:
+    import win32crypt
+else:
+    from Crypto.Cipher import AES
+    from Crypto.Protocol.KDF import PBKDF2
 
 from os.path import expanduser
 
@@ -20,15 +28,19 @@ def get_cipher():
 
     my_pass = 'peanuts'.encode('utf8')
 
-    # 1003 on Mac, 1 on Linux
     iterations = 1
 
     key = PBKDF2(my_pass, salt, length, iterations)
     return AES.new(key, AES.MODE_CBC, IV=iv)
 
 def encrypt(str):
-    length = 16 - (len(str) % 16)
-    return get_cipher().encrypt(str+chr(length)*length)
+    encrypted = None
+    if windows:
+        encrypted = win32crypt.CryptProtectData(str, None, None, None, None, 0)
+    else:
+        length = 16 - (len(str) % 16)
+        encrypted = 'v10' + get_cipher().encrypt(str+chr(length)*length)
+    return encrypted
 
 def has_cookie(conn, name):
     c = conn.cursor()
@@ -41,7 +53,7 @@ def has_cookie(conn, name):
 
 def update_netflix_id(conn, name, expires_utc, last_access_utc, cookie_data):
     sql = 'UPDATE cookies SET expires_utc=?, last_access_utc=?, encrypted_value=? WHERE host_key = ? and name = ?'
-    parms = (expires_utc, last_access_utc, 'v10'+encrypt(cookie_data), '.netflix.com',name)
+    parms = (expires_utc, last_access_utc, encrypt(cookie_data), '.netflix.com',name)
     cur = conn.cursor()
     cur.execute(sql, parms)
     conn.commit()
@@ -52,7 +64,7 @@ def insert_netflix_id(conn, name, expires_utc, last_access_utc, cookie_data, onl
     sec = 0
     if only_secure == True:
         sec = 1
-    parms = (creation_utc, '.netflix.com', name, '', '/', expires_utc, sec, 0, last_access_utc, 1, 1, 1, 'v10'+encrypt(cookie_data), 0)
+    parms = (creation_utc, '.netflix.com', name, '', '/', expires_utc, sec, 0, last_access_utc, 1, 1, 1, encrypt(cookie_data), 0)
     cur = conn.cursor()
     cur.execute(sql, parms)
     conn.commit()
@@ -69,7 +81,13 @@ def set_cookie(conn, name, value, expires, only_secure = False):
 
 
 def set_netflix_cookies(netflix_id, netflix_id_expires, secure_netflix_id, secure_netflix_id_expires):
-    conn = sqlite3.connect(expanduser("~")+'/.config/google-chrome/Default/Cookies')
+    utility.log(expanduser("~"))
+    db_path = expanduser("~")
+    if windows:
+        db_path += '\AppData\Local\Google\Chrome\User Data\Default\Cookies'
+    else:
+        db_path += '/.config/google-chrome/Default/Cookies'
+    conn = sqlite3.connect(db_path)
     conn.text_factory = str
     if netflix_id != None:
         set_cookie(conn, 'NetflixId', netflix_id, netflix_id_expires)
