b643bb289b10422eee99e4fa9d75bd1ca2679fda
logi85 <t.ribke@gmail.com>
2015-12-30 23:48:59 +0100

    added init

--- copy2repo.sh
+++ copy2repo.sh
@@ -4,3 +4,5 @@ cp addon.xml ../logi85.github.io/plugin.video.flix2kodi/
 cp icon.png ../logi85.github.io/plugin.video.flix2kodi/
 cp fanart.jpg ../logi85.github.io/plugin.video.flix2kodi/
 zip -r ../logi85.github.io/plugin.video.flix2kodi/plugin.video.flix2kodi-`python read_version.py`.zip . -x ./.git\* ./copy2repo.sh ./read_version.py ./test\* ./.git* ./.idea\*
+cd ../logi85.github.io/
+/usr/bin/python3 init.py
