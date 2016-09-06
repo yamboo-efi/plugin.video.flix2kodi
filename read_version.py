f5c15151919e46c41e551fcb0687fab93d8f31aa
logi85 <t.ribke@gmail.com>
2016-09-06 22:32:16 +0200

    added windows support

--- read_version.py
+++ read_version.py
@@ -1,5 +1,5 @@
 import xmltodict
 
 with open('plugin.video.flix2kodi/addon.xml') as fd:
-    doc = xmltodict.parse(fd.read())
-    print doc['addon']['@version']
+	doc = xmltodict.parse(fd.read())
+	print doc['addon']['@version']+'fefe'
