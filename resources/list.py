68b60b1be40db057abd8686d55a9945317964819
Francisco Blas (klondike) Izquierdo Riera <klondike@klondike.es>
2018-01-06 22:47:12 +0100

    Fix Superbrowse on LibreELEC
    Until now a relative path was used to access the superbrowse_categories.json file.
    Instead make use of xbmc.translatePath and generic_utility.addon_dir to generate the path to the file correctly so superbrowse works again on LibreELEC and other systems.

--- resources/list.py
+++ resources/list.py
@@ -193,7 +193,7 @@ def genres(video_type):
 def superbrowse():
     xbmcplugin.addSortMethod(plugin_handle, xbmcplugin.SORT_METHOD_LABEL)
 
-    with open(".kodi/addons/plugin.video.flix2kodi/resources/data/superbrowse_categories.json") as categories_file:
+    with open(xbmc.translatePath(generic_utility.addon_dir()+'/resources/data/superbrowse_categories.json')) as categories_file:
         categories = json.load(categories_file)
 
     for category_name,category_id in categories.iteritems():
